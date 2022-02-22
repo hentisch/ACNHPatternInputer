95970efbdd66245115f6958550715c4f6052b820
Henry Tischler <mysterykeven@gmail.com>
2022-02-21 17:41:32 -0700

    Added method to change color index

--- Controller.py
+++ Controller.py
@@ -1,3 +1,4 @@
+import random as rand
 import nxbt
 import time
 import sys
@@ -10,6 +11,7 @@ class Controller():
         self.nx = nxbt.Nxbt()
         self.pro_controller = self.nx.create_controller(nxbt.PRO_CONTROLLER)
         self.nx.wait_for_connection(self.pro_controller)
+        self.total_index_changes = 0 #This mod 16 will give the current color index
     
     def reset_canvas_pos(self) -> None:
         for x in range(16):
@@ -47,19 +49,45 @@ class Controller():
             self.nx.press_buttons(self.pro_controller, [nxbt.Buttons.R])
             self.nx.press_buttons(self.pro_controller, [nxbt.Buttons.DPAD_DOWN])
         self.nx.press_buttons(self.pro_controller, [nxbt.Buttons.A])
+    
+    def change_color(self, color_index:int) -> None:
+        if color_index > 15:
+            raise ValueError("Color index cannot be larger than 15")
+            
+        normal_change = color_index - self.total_index_changes % 16
+        #wrapped_change = min(15 - color_index, color_index)
+        change = normal_change #Change this to select the most efficent method
+        if change > 0:
+            action = nxbt.Buttons.R
+        else:
+            action = nxbt.Buttons.L
+        
+        for x in range(abs(change)):
+            self.nx.press_buttons(self.pro_controller, [action]) #Moving to brightness slider
+        
+        self.total_index_changes += change
 
 
 
 def main():
-
     control = Controller()
     pattern = Pattern.load_from_file(sys.argv[1])
     input("Press enter to continue with script execution: ")
     control.reset_canvas_pos()
     time.sleep(1)
-    control.adjust_palette(pattern.palette) #After this, press A to exit the menu
+    #control.adjust_palette(pattern.palette) #After this, press A to exit the menu
     time.sleep(1)
-    control.select_pencil_from_color_tool()
+    #control.select_pencil_from_color_tool()
+    while True:
+        num = rand.randint(0, 15)
+        print("Moving color to " + str(num))
+        control.change_color(num)
+        time.sleep(5)
+    control.change_color(4)
+    control.change_color(7)
+    control.change_color(13)
+    control.change_color(9)
+    control.change_color(14)
 
 
 if __name__ == "__main__":
