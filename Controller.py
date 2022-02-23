007a4944005928005fffc5d3ae831ff5cc841887
Henry Tischler <mysterykeven@gmail.com>
2022-02-23 01:11:01 -0700

    Clean code

--- Controller.py
+++ Controller.py
@@ -1,5 +1,4 @@
 import random as rand
-from textwrap import wrap
 import nxbt
 import time
 import sys
@@ -11,10 +10,12 @@ class Controller():
     def __init__(self) -> None:
         self.nx = nxbt.Nxbt()
         self.pro_controller = self.nx.create_controller(nxbt.PRO_CONTROLLER)
-        self.nx.wait_for_connection(self.pro_controller)
+
         self.total_index_changes = 0 #This mod 16 will give the current color index
         self.xPos = 0
         self.yPos = 0
+
+        self.nx.wait_for_connection(self.pro_controller)
     
     def reset_canvas_pos(self) -> None:
         for x in range(16):
@@ -30,21 +31,20 @@ class Controller():
         self.xPos = 0 
         self.yPos = 0
     
-    def get_point_sign(self, current_point:int, destination_point:int) -> None:
+    def get_point_sign(self, current_point:int, destination_point:int) -> int:
         if destination_point < current_point:
             return 1
-        else:
-            return -1
+        return -1
     
-    def get_point_distance(self, destination_point:int, current_point:int) -> None:
+    def get_point_distance(self, destination_point:int, current_point:int) -> int:
         return abs(destination_point - current_point) * self.get_point_sign(destination_point, current_point)
 
     def move_to_location(self, x_point:int, y_point:int) -> None:
         xTravel = self.get_point_distance(x_point, self.xPos)
         yTravel = self.get_point_distance(y_point, self.yPos)
-        print(xTravel)
-        print(yTravel)
+
         for x in range(max(abs(xTravel), abs(yTravel))):
+
             if x < abs(xTravel):
                 if xTravel > 0:
                     self.nx.press_buttons(self.pro_controller, [nxbt.Buttons.DPAD_RIGHT])
@@ -60,7 +60,6 @@ class Controller():
                 else:
                     self.nx.press_buttons(self.pro_controller, [nxbt.Buttons.DPAD_UP])
                     self.yPos -= 1
-
     
     def select_color_tool(self) -> None:
         self.nx.press_buttons(self.pro_controller, [nxbt.Buttons.X])
@@ -103,6 +102,7 @@ class Controller():
             raise ValueError("Color index cannot be larger than 15")
             
         normal_change = color_index - self.total_index_changes % 16
+
         #to find wraped change, go the other way from the normal change
         if normal_change > 0:
             wrapped_change = (self.total_index_changes % 16 * -1) + 1 + ((15 - color_index)*-1) -2 
@@ -110,7 +110,7 @@ class Controller():
             wrapped_change =  (15 - self.total_index_changes % 16) + 1 + color_index
         
         if abs(normal_change) <= abs(wrapped_change):
-            change = normal_change #Change this to select the most efficent method
+            change = normal_change
         else:
             change = wrapped_change
         
@@ -120,7 +120,7 @@ class Controller():
             action = nxbt.Buttons.L
         
         for x in range(abs(change)):
-            self.nx.press_buttons(self.pro_controller, [action], down=0.3) #Moving to brightness slider
+            self.nx.press_buttons(self.pro_controller, [action], down=0.3)
             time.sleep(0.3)
         
         self.total_index_changes += change
@@ -129,22 +129,28 @@ class Controller():
         self.change_color(color_index)
         self.nx.press_buttons(self.pro_controller, [nxbt.Buttons.A])
     
-    def fill_pattern(self, pattern:'list[list[int]]'):
+    def fill_pattern(self, pattern:'list[list[int]]') -> None:
+
         for i, r in enumerate(pattern):
-            if i % 2 == 0: #even
-                for c in r:
-                    self.fill_pixel(c)
-                    self.nx.press_buttons(self.pro_controller, [nxbt.Buttons.DPAD_RIGHT])
-                    time.sleep(0.2)
-                    self.xPos += 1
-                    print(f"X: {self.xPos}, Y: {self.yPos}")
+            x_increment:int
+            action:nxbt.Buttons
+            reverse_function:function
+
+            if i % 2 == 0:
+                x_increment = 1
+                action = nxbt.Buttons.DPAD_RIGHT
+                reverse_function = lambda x: x
             else:
-                for c in reversed(r):
-                    self.fill_pixel(c)
-                    self.nx.press_buttons(self.pro_controller, [nxbt.Buttons.DPAD_LEFT])
-                    time.sleep(0.2)
-                    self.xPos -= 1
-                    print(f"X: {self.xPos}, Y: {self.yPos}")
+                x_increment = -1
+                action = nxbt.Buttons.DPAD_LEFT
+                reverse_function = reversed
+            
+            for c in reverse_function(r):
+                self.fill_pixel(c)
+                self.nx.press_buttons(self.pro_controller, [action])
+                time.sleep(0.2)
+                self.xPos += x_increment
+            
             self.nx.press_buttons(self.pro_controller, [nxbt.Buttons.DPAD_DOWN])
             self.yPos += 1
     
@@ -154,12 +160,12 @@ class Controller():
         while True:
             count += 1
             num = rand.randint(0, 15)
+
             print(f"Adjusting color to index {num} from {last_num} | {count} changes have been done in total")
             self.change_color(num)
             last_num = num
+            
             time.sleep(1)
-            if rand.random() < 0.1:
-                time.sleep(5)
 
 def main():
     control = Controller()
@@ -167,11 +173,14 @@ def main():
     input("Press enter to continue with script execution: ")
     control.reset_canvas_pos()
     time.sleep(1)
+
     control.adjust_palette(pattern.palette) #After this, press A to exit the menu
     time.sleep(1)
     control.select_pencil_from_color_tool()
     time.sleep(1)
+
     control.fill_pattern(pattern.pattern_matrix)
+    print("Done!")
 
 if __name__ == "__main__":
     main()
